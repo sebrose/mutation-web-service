583393beff09c23f84ce4810b3f81675bcec47cb
Markus Schirp <mbj@schirp-dso.com>
2014-03-05 15:07:11 +0000

    Add boilerplate for ruby client

---
+++ client-ruby/lib/checkout/cli.rb
@@ -0,0 +1,21 @@
+module Checkout
+  # CLI client
+  class CLI
+
+    EXIT_FAILURE = 1
+    EXIT_SUCCESS = 0
+
+    # Call command line interface
+    #
+    # @param [Array<String>] arguments
+    #
+    # @return [Fixnum]
+    #   the exit status
+    #
+    # @api private
+    #
+    def self.run(arguments)
+    end
+
+  end # CLI
+end # Checkout
